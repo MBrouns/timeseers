699e218f9fec94b5233527a88478630fbd7a611c
Koray Beyaz <koray.beyaz73@gmail.com>
2021-03-15 21:21:15 +0000

    fix unit tests

--- tests/test_fourier_seasonality.py
+++ tests/test_fourier_seasonality.py
@@ -9,6 +9,6 @@ def test_can_fit_generated_data(seasonal_data):
     model.fit(data, data["value"], y_scaler=IdentityScaler, tune=1000, draws=10000)
 
     # No groups, so we can take mean over 1 as well
-    model_beta = np.mean(model.trace_["beta"], axis=(0, 1))
+    model_beta = np.mean(model.trace_[model._param_name("beta")], axis=(0, 1))
 
     np.testing.assert_allclose(model_beta, true_beta, atol=0.12)
--- tests/test_logistic_growth.py
+++ tests/test_logistic_growth.py
@@ -6,6 +6,6 @@ import numpy as np
 def test_can_fit_generated_data(logistic_growth_data):
     data, true_delta, n_changepoints = logistic_growth_data
     model = LogisticGrowth(capacity=1, n_changepoints=n_changepoints)
-    model.fit(data, data["value"], cores=1, chains=1, y_scaler=MaxScaler)
-    model_delta = model.trace_[model._param_name("delta")].mean(axis=0)[0]
+    model.fit(data, data["value"], y_scaler=MaxScaler)
+    model_delta = model.trace_[model._param_name("delta")].mean(axis=(0, 1))
     np.testing.assert_allclose(model_delta, true_delta, atol=0.01)
