455ee55c957ef37bdc76b0b1b5520c813f211fc0
Will Dean <wdean@homepartners.com>
2021-05-31 09:42:30 -0500

    changing test name to match style of others. add test to compare fit parameters of two different sets of weights

--- tests/test_likelihood.py
+++ tests/test_likelihood.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 from timeseers import Constant
-from timeseers.utils import IdentityScaler
+from timeseers import utils
 
 def weighted_sigma(y, sample_weight):
     n_observations = len(y)
@@ -21,13 +21,31 @@ def weighted_mean(y, sample_weight):
     return (y * sample_weight).sum() / sample_weight.sum()
 
 
-def test_weights(weighted_data):
+def test_can_fit_generated_data(weighted_data):
     df = weighted_data
 
-    model = Constant(name="intercept", lower=-1, upper=1)
-    model.fit(df[["t"]], df["value"], sample_weight=df['weight'], y_scaler=IdentityScaler, tune=2000)
+    model = Constant(lower=-1, upper=1)
+    model.fit(df[["t"]], df["value"], sample_weight=df['weight'], y_scaler=utils.IdentityScaler, tune=2000)
 
     sigma_estimate = model.trace_["sigma"].mean()
     sigma_true = weighted_sigma(df["value"], df["weight"])
 
     np.testing.assert_allclose(sigma_estimate, sigma_true, atol=.01)
+
+
+def test_generated_data_parameter_change():
+    df_lower_weights = utils.weighted_data(weights=[1, 2])
+    # Higher weight on larger sigma data
+    df_higher_weights = utils.weighted_data(weights=[1, 5])
+
+    model_lower = Constant(lower=-1, upper=1)
+    model_higher = Constant(lower=-1, upper=1)
+
+    model_lower.fit(df_lower_weights[["t"]],
+    df_lower_weights["value"], sample_weight=df_lower_weights['weight'], y_scaler=utils.IdentityScaler, tune=2000)
+    model_higher.fit(df_higher_weights[["t"]], df_higher_weights["value"], sample_weight=df_higher_weights['weight'], y_scaler=utils.IdentityScaler, tune=2000)
+
+    sigma_lower = model_lower.trace_["sigma"].mean()
+    sigma_higher = model_higher.trace_["sigma"].mean()
+
+    assert sigma_lower < sigma_higher
