6ccc372d9a189a4ad8799c792afb405ba8727f64
Sven Thies <svenandreas.x.thies@viivhealthcare.com>
2023-03-17 16:33:02 +0100

    remove some code.

--- src/main.py
+++ src/main.py
@@ -6,41 +6,8 @@ from timeseers.fourier_seasonality import FourierSeasonality
 from timeseers.regressor import Regressor
 import matplotlib.pyplot as plt
 
-
-# constants
-SPLIT_DATE = '2022-07-01'
-
-# load data
-df = pd.read_csv('../input/data/dovato_sales.csv', index_col=False).assign(
-    t=lambda d: pd.to_datetime(d['date_month']),
-    value=lambda d: d['days_of_treatment']
-)
-df.rename({'gross_segment':'g'}, axis=1, inplace=True)
-df = df[['t','g','value']]
-
-# select groups and order
-dct_gs = {
-    7226: 'muenchen_maxvorstadt', 
-    7228: 'muenchen_altstadt',    
-    7212: 'nuernberg_nord_fuerth',
-    7231: 'regensburg',            
-    7211: 'erlangen',             
-    7218: 'augsburg',             
-    7213: 'nuernberg_sued',       
-    7204: 'wuerzburg',            
-    7235: 'landshut',
-    7225: 'muenchen_laim',
-    7233: 'rosenheim',
-    7219: 'diedorf',
-    7221: 'kempten',
-    7237: 'trostberg'
-}
-cust_gs = []#[7228,7212,7213,7231,7204,7205]
-if cust_gs:
-    gs = cust_gs
-else:
-    gs = list(dct_gs.keys())
-df = df[df.g.isin(gs)].copy().reset_index(drop=True)
+# load data into dataframe
+# / 
 
 # add corona data as external regressor
 df_corona = pd.read_csv('../input/data/data_corona_oxford_ger.csv', index_col=False)
@@ -80,5 +47,3 @@ preds = model.predict(X_train, X_test)
 
 # plot components (calls predict internally)
 model.plot_components(X_train, y_train, X_test, y_test, groups=df['g'])
-
-print("finished")
